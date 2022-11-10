6455bee65c7183837f0cef250ed06f6666d46184
hawik <hawiksyaef@gmail.com>
2022-11-10 10:26:38 +0700

    konfigurasi koneksi db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import db from "./config/db.js";
 
+// deklarasi express
 const app = express();
 
+// cek konseksi database
+try {
+  await db.authenticate();
+  console.log("DB Connected!");
+} catch (error) {
+  console.log(error);
+}
+
+// cek server running
 app.listen(5000, () => console.log("Server up and running!"));
