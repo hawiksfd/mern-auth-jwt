edfd5994472c7f9d5c692f65b96faa1aa8799804
hawik <hawiksyaef@gmail.com>
2022-11-13 14:01:06 +0700

    konfigurasi login with jwt & bcrypt

--- backend/Controllers/UserControl.js
+++ backend/Controllers/UserControl.js
@@ -46,3 +46,61 @@ export const Register = async (req, res) => {
     console.log(error);
   }
 };
+
+export const Login = async (req, res) => {
+  try {
+    // cek akun by email
+    const user = await Users.findAll({
+      where: { email: req.body.email },
+    });
+
+    // cek password
+    const match = await bcrypt.compare(req.body.password, user[0].password);
+
+    if (!match) return res.status(400).json({ msg: "Password anda salah!" });
+
+    // constract field user
+    const userId = user[0].id;
+    const name = user[0].name;
+    const email = user[0].email;
+
+    // membuat access token
+    const accessToken = jsonwebtoken.sign(
+      { userId, name, email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "20s",
+      }
+    );
+
+    // membuat refresh token
+    const refreshToken = jsonwebtoken.sign(
+      { userId, name, email },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // update refresh token db
+    await Users.update(
+      { refresh_token: refreshToken },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+
+    // setting refresh token pada cookie
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    // kirim response ke client access token
+    res.json({ accessToken });
+  } catch (error) {
+    res.status(404).json({ msg: "Akun anda tidak ditemukan!" });
+  }
+};
--- backend/Routes/UserRoute.js
+++ backend/Routes/UserRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers, Register } from "../Controllers/UserControl.js";
+import { getUsers, Register, Login } from "../Controllers/UserControl.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/register", Register);
+router.post("/login", Login);
 
 export default router;
